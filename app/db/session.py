a6214f4b055ea0d722a5f834fbab2f96dfe95ad2
swiftkeyy <80327899+swiftkeyy@users.noreply.github.com>
2026-04-11 19:50:15 +0400

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -2,11 +2,7 @@ from __future__ import annotations
 
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import get_settings
 
