fb2a804024a0343165a887451983b6da2a0f3201
swiftkeyy <80327899+swiftkeyy@users.noreply.github.com>
2026-04-12 21:40:37 +0400

    Update callbacks.py

--- app/utils/callbacks.py
+++ app/utils/callbacks.py
@@ -21,3 +21,10 @@ class AdminActionCb(CallbackData, prefix="adm"):
     action: str
     entity_id: int | None = None
     page: int | None = None
+
+
+class AdminCb(CallbackData, prefix="adm"):
+    section: str
+    action: str
+    entity_id: int | None = None
+    page: int | None = None
