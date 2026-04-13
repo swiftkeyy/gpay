85ad3b2d1fecc18d386059890abeb55a439c4106
swiftkeyy <80327899+swiftkeyy@users.noreply.github.com>
2026-04-13 21:58:11 +0400

    Update db.py

--- app/middlewares/db.py
+++ app/middlewares/db.py
@@ -18,4 +18,10 @@ class DbSessionMiddleware(BaseMiddleware):
     ) -> Any:
         async with SessionLocal() as session:
             data["session"] = session
-            return await handler(event, data)
+            try:
+                result = await handler(event, data)
+                await session.commit()
+                return result
+            except Exception:
+                await session.rollback()
+                raise
