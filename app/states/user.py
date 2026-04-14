ffa4b1f1c62e2bb6c60e2bb769cfdbb584057809
swiftkeyy <80327899+swiftkeyy@users.noreply.github.com>
2026-04-14 14:24:46 +0400

    Add files via upload

--- app/states/user.py
+++ app/states/user.py
@@ -6,3 +6,29 @@ class CheckoutStates(StatesGroup):
     entering_promo = State()
     leaving_review = State()
     contacting_support = State()
+
+
+class SellerStates(StatesGroup):
+    waiting_shop_name = State()
+    waiting_shop_description = State()
+    waiting_lot_title = State()
+    waiting_lot_description = State()
+    waiting_lot_price = State()
+    waiting_stock_items = State()
+    waiting_withdrawal_amount = State()
+    waiting_withdrawal_details = State()
+
+
+class DealStates(StatesGroup):
+    waiting_message = State()
+    waiting_dispute_reason = State()
+    waiting_delivery_data = State()
+
+
+class LotCreationStates(StatesGroup):
+    selecting_product = State()
+    entering_title = State()
+    entering_description = State()
+    entering_price = State()
+    selecting_delivery_type = State()
+    entering_stock_items = State()
